a7197a2fafa3391d3d1a9d2d6491bfdb88556d54
Andre <andrebibal@gmail.com>
2025-08-16 05:41:35 +0000

    edited

---
+++ OddNumberFinder.java.java/Oddnumberfiner.java
@@ -0,0 +1,36 @@
+/**
+* EvenNumberFinder.java
+* This is the "worker" class. It contains the core logic for
+* finding and displaying even numbers within a given range.
+* It is separated from the main application to improve organization and reusability.
+*/
+public class OddNumberFinder {
+ 
+    /**
+     * Iterates through a specified range and prints all even numbers.
+     * @param start The starting number of the range.
+     * @param end The ending number of the range.
+     */
+    public void findAndDisplayOdds(int start, int end) {
+        System.out.println("\nEven numbers in the range " + start + " to " + end + " are:");
+ 
+        // Use a for loop to iterate from the start point to the end point
+        for (int i = start; i <= end; i++) {
+            // Check if the current number (i) is even.
+            // A number is even if the remainder when divided by 2 is 0.
+            if (i % 2 != 0) {
+                // If the number is even, print it to the console
+                System.out.println(i);
+            }
+        }
+    }
+//add the implementation of finding odd numbers in the given range but using
+//while loop
+
+/* syntax of while loop
+    initialization;
+    while(condition) {
+        statements;
+        incrementation;
+    }
+}        
